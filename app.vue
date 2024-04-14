f1f210c7a29ae385b0a71f39e70f474267e2cdc7
Cédric Pouilleux <mino.mnz@gmail.com>
2024-04-14 18:01:23 +0200

    Add linter, google font

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    TEST CA
+  <div class="center">
+    <h1>equiloot</h1>
   </div>
 </template>
+
+<style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+<style scoped lang="scss">
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+}
+h1 {
+  font-family: Lato;
+  margin: 0;
+  padding: 0;
+  color: #333;
+  font-size: 3em;
+}
+</style>
